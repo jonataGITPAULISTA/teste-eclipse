7ae24994d7792f11770aca1230837ea921d2f3f7
jonataGITPAULISTA <jonata3015@gmail.com>
2025-04-01 18:28:43 -0300

    commit do vscode

--- testeeclipse/src/dio/jonata/java/Principal.java
+++ testeeclipse/src/dio/jonata/java/Principal.java
@@ -3,7 +3,9 @@ package dio.jonata.java;
 public class Principal {
 	public static void main(String[] args) {
 		Aluno primeiroAluno = new Aluno();
-		
+		Aluno segundoAluno = new Aluno();
+
+		segundoAluno.setNome("pedro");
 		primeiroAluno.setNome("cassio");
 		System.out.println(primeiroAluno.getNome());
 	
