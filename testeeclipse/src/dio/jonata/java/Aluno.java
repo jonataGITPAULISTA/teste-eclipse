d7064f9e1c6eeb942e7c559b7f71d66c8ca3882c
jonataGITPAULISTA <jonata3015@gmail.com>
2025-04-01 18:23:28 -0300

    Primeiro commit

---
+++ testeeclipse/src/dio/jonata/java/Aluno.java
@@ -0,0 +1,20 @@
+package dio.jonata.java;
+
+public class Aluno {
+	String nome="";
+	int idadeAluno=0;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdadeAluno() {
+		return idadeAluno;
+	}
+	public void setIdadeAluno(int idadeAluno) {
+		this.idadeAluno = idadeAluno;
+	}
+	
+	
+}
---
+++ testeeclipse/src/dio/jonata/java/Principal.java
@@ -0,0 +1,11 @@
+package dio.jonata.java;
+
+public class Principal {
+	public static void main(String[] args) {
+		Aluno primeiroAluno = new Aluno();
+		
+		primeiroAluno.setNome("cassio");
+		System.out.println(primeiroAluno.getNome());
+	
+	}
+}
